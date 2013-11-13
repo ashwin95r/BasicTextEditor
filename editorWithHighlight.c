5c46b92bedf2864f614e874c6c5d3dd4a36e857f
ashwin <ashwin@ashwin-pc.(none)>
2013-11-13 22:07:42 +0530

    5

--- editorWithHighlight.c
+++ editorWithHighlight.c
@@ -1,37 +1,70 @@
+/* 
+ *	Text editor project
+ *
+ *	Syntax Terror
+ */
+
+
+
 #include <gtk/gtk.h>
+#include <stdio.h>
+
+/* List of functions and their corresponding tool tips. */
+static char *tips[][2] = 
+  {
+    {"printf", "(const char *format, ...)"},
+    {"fprintf", "(FILE *stream, const char *format, ...)"},
+    {"scanf", "(const char *format, ...)"},
+    {"snprintf", "(char *str, size_t size, const char *format, ...)"},
+    {"fputc", "(int c, FILE *stream)"},
+    {"fputs", "(const char *s, FILE *stream)"},
+    {"putc", "(int c, FILE *stream)"},
+    {"putchar", "(int c)"},
+    {"puts", "(const char *s)"},
+		{"for","(initialisation; ending condition; counter)"},  
+		{"if", "(condition)"},
+		{"else if","(condition)"},
+		{"while","(condition)"},
+	};
+
+#define NUM_TIPS (sizeof (tips) / sizeof (tips[0]))
 
 typedef struct
 {
   GtkWidget *textview, *search;
 } TextEditor;
 
+GtkTextTagTable *table1;
+GtkTextTag *highlight_tag;
 
-
-void
-search_button_clicked (GtkWidget *search_button, TextEditor *editor)
+void search_button_clicked (GtkWidget *search_button, TextEditor *editor)
 {
-  const gchar *text;
+	if(GTK_IS_TEXT_VIEW (editor->textview)&&!GTK_IS_TEXT_TAG_TABLE (table1)){  
+	const gchar *text;
   GtkTextBuffer *buffer;
   GtkTextIter iter;
   GtkTextIter mstart, mend; 
   gboolean found;
-	gchar text_highlight;	
-	GtkTextTag *highlight_tag;
+	//gchar text_highlight;	
+	
 
   text = gtk_entry_get_text (GTK_ENTRY (editor->search));
 
   /* Get the buffer associated with the text view widget. */
   buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (editor->textview));
   /* Search from the start from buffer for text. */
-  gtk_text_buffer_get_start_iter (buffer, &iter);
+  
+	gtk_text_buffer_get_start_iter (buffer, &iter);
   found = gtk_text_iter_forward_search (&iter, text, 0, &mstart, &mend, NULL);
 	
-	highlight_tag = gtk_text_buffer_create_tag(buffer, "highlight_text",
-    							 "foreground", "blue", NULL);	
+	if(GTK_IS_TEXT_TAG_TABLE (table1))
+		gtk_text_tag_table_remove(table1,highlight_tag);
+
+	highlight_tag = gtk_text_buffer_create_tag(buffer, NULL,"foreground", "blue", NULL);	
 	
-	void gtk_text_buffer_remove_all_tags( GtkTextBuffer     *buffer,
-                                      const GtkTextIter *start,
-                                      const GtkTextIter *end );
+	if(GTK_IS_TEXT_TAG_TABLE (table1))
+		gtk_text_tag_table_add(table1, highlight_tag);
+
 	while(found == 1){
 	
 	found = gtk_text_iter_forward_search (&iter, text, 0, &mstart, &mend, NULL);
@@ -39,29 +72,62 @@ search_button_clicked (GtkWidget *search_button, TextEditor *editor)
   if (found){
       /* If found, highlight the text. */
   //  gtk_text_buffer_select_range (buffer, &mstart, &mend);
-	//gtk_text_buffer_apply_tag(buffer, highlight_tag, &mstart, &mend);	
-	gtk_text_buffer_apply_tag_by_name( buffer,"highlight_text",&mstart,&mend );	
+	gtk_text_buffer_apply_tag(buffer, highlight_tag, &mstart, &mend);	
 	iter = mend;
 	}
-
 	}
-	
+	}
 }
 
-void
-font_highlight ( TextEditor *editor)
+void font_highlight ( TextEditor *editor)
 {
-  const gchar *text;
+
+	g_print("enter");  
+	 gchar *text;
   GtkTextBuffer *buffer;
   GtkTextIter iter;
   GtkTextIter mstart, mend; 
   gboolean found;
-	gchar text_highlight;	
+	//gchar text_highlight;	
  	GtkTextTag *highlight_tag2;	
   text = "for";
 
+	//scanf("%s",text);
+
   /* Get the buffer associated with the text view widget. */
   buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (editor->textview));
+  /* Search from the start from buffer for text. */
+  
+	if(GTK_IS_TEXT_BUFFER (buffer))
+{
+	if(!GTK_IS_TEXT_VIEW (editor->textview)&&!GTK_IS_TEXT_TAG_TABLE (table1)){
+	gtk_text_buffer_get_start_iter (buffer, &iter);
+  	
+	found = gtk_text_iter_forward_search (&iter, text, 0, &mstart, &mend, NULL);
+	
+	highlight_tag2 = gtk_text_buffer_create_tag(buffer, "blue_fg", 
+							 "foreground", "blue", NULL);
+	
+
+	/*while(found == 1){
+	
+	found = gtk_text_iter_forward_search (&iter, text, 0, &mstart, &mend, NULL);
+
+  if (found){
+      /* If found, highlight the text. */
+  //  gtk_text_buffer_select_range (buffer, &mstart, &mend);
+	/*gtk_text_buffer_apply_tag_by_name( buffer,"blue_fg",&mstart,&mend );	
+	iter = mend;
+g_print("enter");
+*/	
+}
+
+
+	else{
+	
+
+  /* Get the buffer associated with the text view widget. */
+  //buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (editor->textview));
   /* Search from the start from buffer for text. */
   gtk_text_buffer_get_start_iter (buffer, &iter);
   found = gtk_text_iter_forward_search (&iter, text, 0, &mstart, &mend, NULL);
@@ -79,10 +145,12 @@ font_highlight ( TextEditor *editor)
   //  gtk_text_buffer_select_range (buffer, &mstart, &mend);
 	gtk_text_buffer_apply_tag_by_name( buffer,"blue_fg",&mstart,&mend );	
 	iter = mend;
+	g_print("enter");
 	}
 
 	}
-	
+	}
+}
 }
 
 
@@ -96,6 +164,149 @@ static void copy_clicked (GtkToolButton*, TextEditor*);
 static void paste_clicked (GtkToolButton*, TextEditor*);
 static void find_clicked (GtkButton*, TextEditor*);
 void undo_redo(GtkWidget *widget,  gpointer item);
+
+gchar *
+get_tip(gchar *text)
+{
+  gint i;
+  gboolean found;
+
+  found = FALSE;
+  for (i = 0; i < NUM_TIPS; i++)
+    {
+      if (strcmp (text, tips[i][0]) == 0)
+        {
+          found = TRUE;
+          break;
+        }
+    }
+  if (!found)
+    return NULL;
+
+  return g_strdup (tips[i][1]);
+}
+
+GtkWidget *
+tip_window_new (gchar *tip)
+{
+  GtkWidget *win;
+  GtkWidget *label;
+  GtkWidget *eb;
+  GdkColormap *cmap;
+  GdkColor color;
+  PangoFontDescription *pfd;
+
+  win = gtk_window_new (GTK_WINDOW_POPUP);
+  gtk_container_set_border_width (GTK_CONTAINER (win), 0);
+
+  eb = gtk_event_box_new ();
+  gtk_container_set_border_width (GTK_CONTAINER (eb), 1);
+  gtk_container_add (GTK_CONTAINER (win), eb);
+
+  label = gtk_label_new (tip);  
+  gtk_container_add (GTK_CONTAINER (eb), label);
+
+  pfd = pango_font_description_from_string ("courier");
+  gtk_widget_modify_font (label, pfd);
+  
+  cmap = gtk_widget_get_colormap (win);
+  color.red = 0;
+  color.green = 0;
+  color.blue = 0;
+  if (gdk_colormap_alloc_color (cmap, &color, FALSE, TRUE))
+    gtk_widget_modify_bg (win, GTK_STATE_NORMAL, &color);
+  else
+    g_warning ("Color allocation failed!\n");
+
+  cmap = gtk_widget_get_colormap (eb);
+  color.red = 65535;
+  color.green = 65535;
+  color.blue = 45535;
+  if (gdk_colormap_alloc_color (cmap, &color, FALSE, TRUE))
+    gtk_widget_modify_bg (eb, GTK_STATE_NORMAL, &color);
+  else
+    g_warning ("Color allocation failed!\n");
+
+  return win;
+}
+
+
+void
+insert_open_brace(GtkWidget **tip_win, GtkWidget *text_view, GtkTextIter *arg1)
+{
+  GdkWindow *win;
+  GtkTextIter start;
+  GdkRectangle buf_loc;
+  gint x, y;
+  gint win_x, win_y;
+  gchar *text;
+  gchar *tip_text;
+  
+  /* Get the word at cursor. */
+  start = *arg1;
+  if (!gtk_text_iter_backward_word_start (&start))
+    return;
+  text = gtk_text_iter_get_text (&start, arg1);
+  g_strstrip (text);
+
+  /* Get the corresponding tooltip. */
+  tip_text = get_tip(text);  
+  if (tip_text == NULL)
+    return;
+  
+  /* Calculate the tool tip window location. */
+  gtk_text_view_get_iter_location (GTK_TEXT_VIEW (text_view), arg1, 
+                                   &buf_loc);
+ // g_printf ("Buffer: %d, %d\n", buf_loc.x, buf_loc.y);
+  gtk_text_view_buffer_to_window_coords (GTK_TEXT_VIEW (text_view),
+                                         GTK_TEXT_WINDOW_WIDGET,
+                                         buf_loc.x, buf_loc.y,
+                                         &win_x, &win_y);
+  //g_printf ("Window: %d, %d\n", win_x, win_y);
+  win = gtk_text_view_get_window (GTK_TEXT_VIEW (text_view), 
+                                  GTK_TEXT_WINDOW_WIDGET);
+  gdk_window_get_origin (win, &x, &y);
+
+  /* Destroy any previous tool tip window. */
+  if (*tip_win != NULL)
+    gtk_widget_destroy (GTK_WIDGET (*tip_win));  
+  
+  /* Create a new tool tip window and place it at the caculated
+     location. */
+  *tip_win = tip_window_new (tip_text);
+  g_free(tip_text);
+  gtk_window_move (GTK_WINDOW (*tip_win), win_x + x, 
+                   win_y + y + buf_loc.height);
+  gtk_widget_show_all (*tip_win);
+}
+
+void
+insert_close_brace (GtkWidget **tip_win)
+{
+  if (*tip_win != NULL)
+    {
+      gtk_widget_destroy (GTK_WIDGET (*tip_win));
+      *tip_win = NULL;
+    }
+}
+
+void
+buffer_insert_text (GtkTextBuffer *textbuffer, GtkTextIter *arg1,
+                    gchar *arg2, gint arg3, gpointer user_data)
+{
+  static GtkWidget *tip_win = NULL;
+
+  if (strcmp (arg2, "(") == 0)
+    {
+      insert_open_brace(&tip_win, GTK_WIDGET (user_data), arg1);
+    }
+
+  if (strcmp (arg2, ")") == 0)
+    {
+      insert_close_brace(&tip_win);
+    }
+	
+}
 
 int main (int argc,
           char *argv[])
@@ -113,19 +324,19 @@ int main (int argc,
 
   GtkTextBuffer *buffer;
 
-
+	PangoFontDescription *pfd;
 	//git add
   
   gtk_init (&argc, &argv);
   
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title (GTK_WINDOW (window), "Syntax Terror Editor");
-  gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+  gtk_container_set_border_width (GTK_CONTAINER (window), 3);
   gtk_widget_set_size_request (window, 600, 500);
   
   g_signal_connect (G_OBJECT (window), "destroy",
                     G_CALLBACK (gtk_main_quit), NULL);
-  
+	  
   editor->textview = gtk_text_view_new ();
   editor->search = gtk_entry_new ();
   find = gtk_button_new_from_stock (GTK_STOCK_FIND);
@@ -136,7 +347,8 @@ int main (int argc,
                     (gpointer) editor);
 
   buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(editor->textview));
-
+	g_signal_connect (G_OBJECT (buffer), "insert_text", 
+                    G_CALLBACK (buffer_insert_text), editor->textview);
   
   toolbar = gtk_toolbar_new();
   gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_ICONS);
@@ -191,7 +403,7 @@ int main (int argc,
         G_CALLBACK(mark_set_callback), statusbar, 0);
 
 //	g_signal_connect(buffer, "changed",
-//       G_CALLBACK(font_highlight), editor);
+//       G_CALLBACK(font_highlight), (gpointer)editor);
 
 
   menubar = gtk_menu_bar_new();
@@ -210,17 +422,24 @@ int main (int argc,
  
   
   scrolled_win = gtk_scrolled_window_new (NULL, NULL);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_win),
+                                  GTK_POLICY_AUTOMATIC,
+                                  GTK_POLICY_AUTOMATIC);
   gtk_container_add (GTK_CONTAINER (scrolled_win), editor->textview);
 
   searchbar = gtk_hbox_new (FALSE, 5);
   gtk_box_pack_start (GTK_BOX (searchbar), editor->search, FALSE, FALSE, 0);
   gtk_box_pack_start (GTK_BOX (searchbar), find, FALSE, FALSE, 0);
-  
+ 	
+/*tooltip*/
+	pfd = pango_font_description_from_string ("courier");
+  gtk_widget_modify_font (editor->textview, pfd);
+/*tooltip*/ 
   vbox = gtk_vbox_new (FALSE, 5);
   gtk_box_pack_start (GTK_BOX (vbox), menubar, FALSE, FALSE, 0);
   gtk_box_pack_start (GTK_BOX (vbox), toolbar, FALSE, FALSE, 0);
-  gtk_box_pack_start (GTK_BOX (vbox), scrolled_win, TRUE, TRUE, 0);
-  gtk_box_pack_start (GTK_BOX (vbox), searchbar, FALSE, FALSE, 0);
+	gtk_box_pack_start (GTK_BOX (vbox), searchbar, FALSE, FALSE, 0);  
+	gtk_box_pack_start (GTK_BOX (vbox), scrolled_win, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(vbox), statusbar, FALSE, FALSE, 0);
 
   gtk_container_add (GTK_CONTAINER (window), vbox);
